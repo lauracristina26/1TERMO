9f9a3381949e2efc561655d678e4570321acb207
Laura Cristina <lauracristinalucas10@gmail.com>
2026-05-12 11:25:55 -0300

    Projeto Cancela 12.05.2026

---
+++ LOPAL/ATIVIDADES/FORMATIVAS/projeto.py
@@ -0,0 +1,42 @@
+# Projeto Cancela Automatica 
+# Criar um algoritmo para que:
+
+# Projeto 1:
+# Projeto: Precisamos de um algoritmo para gerenciamento de cancelas para um shopping.
+# Toda entrada e saída irá ser sinalizada
+# Valores para entrada e permanência do veículo deverá ser pergutado
+# As entrada deverão ser registradas por placa.
+
+# Passo 1:  
+# Perguntar informações sobre o veiculo ou forma acesso
+# Pressionar o botao para emitir ticket
+# Verificar se possui TAG para acesso liberado
+# Se possuir erros informar ao usuário
+
+# Passo 2:
+# Verificar tempo de permanência
+# Valor a ser cobrado
+
+# Passo 3:
+# Saída como será?
+# Calcular tempo de permanência
+# Se for TAG gerar na fatura da TAG
+# Pagar ticket
+# Devolver ticket na saída
+
+# Passo 4:
+# Gerar relatório de entradas e saídas
+# Tratamento de Erros
+# Revisão do código
+
+print("Bem-vindo ao Estacionamento do Shopping!")
+entrada = input("Qual a placa do carro?")
+formaacesso = input("Escolha uma forma de acesso: ")
+
+
+if formaacesso == "Ticket":
+    print("Aperte o botão para emitir o seu Ticket!")
+elif formaacesso == "TAG":
+    print("Acesso liberado! O valor será cobrado na folha de pagamento")
+else:
+    print("Caso ocorra algum erro, recorra ao interfone, estaremos á disposição")
