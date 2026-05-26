40481fe18bd8462c18ac4af8d53310ff4119775a
Laura Cristina <lauracristinalucas10@gmail.com>
2026-05-26 07:55:37 -0300

    Aula 26.05.26

---
+++ LOPAL/AULA01/primeiro.py
@@ -0,0 +1,6 @@
+print("Olá, Mundo!")
+print("Olá, Laura!")
+
+print("Quais seria meus hobbys?")
+print ("Meu hobbys são \n assistir filmes \n ouvir músicas / \n escrever /\n ler")
+print ("Gosto de assistir filmes e ler sobre fantasia, ficções e romance")
